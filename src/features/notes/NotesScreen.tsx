2fd90c662aa50710f4e228888eadad2490d733c2
Anushka Karalkar <anushka.karalkar@parag.com>
2025-11-16 23:32:49 +0530

    Add ChatScreen, NotesScreen, SettingScreen, and TasksScreen components

---
+++ src/features/notes/NotesScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const NotesScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notes Screen</Text>
+    </View>
+  );
+};
+
+export default NotesScreen;
